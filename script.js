9082d12a1ab8d21a0efa89e9ac24cc4c6aef8b54
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-11-05 10:40:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+const ham = document.getElementById('ham');
+const navSub = document.getElementById('nav-sub');
+
+ham.addEventListener('click', () => {
+  navSub.classList.toggle('active');
+  if (ham.classList.toggle('active')) {
+    ham.innerHTML = '&times;'; // cross symbol
+  } else {
+    ham.innerHTML = '&#9776;'; // hamburger icon
+  }
+});
