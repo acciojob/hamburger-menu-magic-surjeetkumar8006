c3e877336aad804e68d145db382949ece90505c3
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-11-05 10:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const ham = document.getElementById('ham');
-const navSub = document.getElementById('nav-sub');
-
-ham.addEventListener('click', () => {
-  navSub.classList.toggle('active');
-  if (ham.classList.toggle('active')) {
-    ham.innerHTML = '&times;'; // cross symbol
-  } else {
-    ham.innerHTML = '&#9776;'; // hamburger icon
-  }
+document.querySelector('.ham').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-sub');
+    if (navLinks.style.display === 'block') {
+        navLinks.style.display = 'none'; // Hide the links
+    } else {
+        navLinks.style.display = 'block'; // Show the links
+    }
 });
